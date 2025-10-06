fa9230d7385738a91a89ad3d08f94bc2a9662a79
David Cris <davidcristhian576@gmail.com>
2025-10-06 20:16:28 -0300

    Alterações no arquivo Contatos.java

--- src/main/java/model/Contatos.java
+++ src/main/java/model/Contatos.java
@@ -2,28 +2,28 @@ package model;
 
 public class Contatos {
     private int id;
-    private int idUsuario;
+    private int id_Usuario;
     private String telefone;
-    private String tituloProfissional;
-    private String linkLinkedin;
-    private String linkPortifolio;
+    private String titulo_Profissional;
+    private String link_Linkedin;
+    private String link_Portifolio;
 
     // Getters e Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
-    public int getIdUsuario() { return idUsuario; }
-    public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
+    public int getIdUsuario() { return id_Usuario; }
+    public void setIdUsuario(int id_Usuario) { this.id_Usuario = id_Usuario; }
 
     public String getTelefone() { return telefone; }
     public void setTelefone(String telefone) { this.telefone = telefone; }
 
-    public String getTituloProfissional() { return tituloProfissional; }
-    public void setTituloProfissional(String tituloProfissional) { this.tituloProfissional = tituloProfissional; }
+    public String getTituloProfissional() { return titulo_Profissional; }
+    public void setTituloProfissional(String titulo_Profissional) { this.titulo_Profissional = titulo_Profissional; }
 
-    public String getLinkLinkedin() { return linkLinkedin; }
-    public void setLinkLinkedin(String linkLinkedin) { this.linkLinkedin = linkLinkedin; }
+    public String getLinkLinkedin() { return link_Linkedin; }
+    public void setLinkLinkedin(String link_Linkedin) { this.link_Linkedin = link_Linkedin; }
 
-    public String getLinkPortifolio() { return linkPortifolio; }
-    public void setLinkPortifolio(String linkPortifolio) { this.linkPortifolio = linkPortifolio; }
+    public String getLinkPortifolio() { return link_Portifolio; }
+    public void setLinkPortifolio(String link_Portifolio) { this.link_Portifolio = link_Portifolio; }
 }
