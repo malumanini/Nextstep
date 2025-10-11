4d876173f84a4275f041bf30a9f100b00a7475b4
David Cris <davidcristhian576@gmail.com>
2025-10-11 15:25:08 -0300

    Criação do arquivo CurriculoDTO.java

---
+++ src/main/java/dto/CurriculoDTO.java
@@ -0,0 +1,23 @@
+package dto;
+
+import java.util.List;
+
+public class CurriculoDTO {
+    private ContatoDTO contato;
+    private String resumo;
+    private List<ExperienciaDTO> experiencias;
+    private List<FormacaoDTO> formacoes;
+    private List<String> habilidades;
+
+    // getters / setters
+    public ContatoDTO getContato() { return contato; }
+    public void setContato(ContatoDTO contato) { this.contato = contato; }
+    public String getResumo() { return resumo; }
+    public void setResumo(String resumo) { this.resumo = resumo; }
+    public List<ExperienciaDTO> getExperiencias() { return experiencias; }
+    public void setExperiencias(List<ExperienciaDTO> experiencias) { this.experiencias = experiencias; }
+    public List<FormacaoDTO> getFormacoes() { return formacoes; }
+    public void setFormacoes(List<FormacaoDTO> formacoes) { this.formacoes = formacoes; }
+    public List<String> getHabilidades() { return habilidades; }
+    public void setHabilidades(List<String> habilidades) { this.habilidades = habilidades; }
+}
