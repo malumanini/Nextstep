6bc3c44628f5faf6daa85452cb45866c70f0a28b
David Cris <davidcristhian576@gmail.com>
2025-10-27 16:09:10 -0300

    Criação do arquivo Idiomas

---
+++ src/main/java/model/Idiomas.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Idiomas {
+    private int id;
+    private int id_usuario;
+    private String nome;
+
+    public Idiomas () {}
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getIdUsuario() { return id_usuario; }
+    public void setIdUsuario(int id_usuario) { this.id_usuario = id_usuario; }
+
+    public String getIdNome() { return nome; }
+    public void setIdNome(String nome) { this.nome = nome; }
+}
